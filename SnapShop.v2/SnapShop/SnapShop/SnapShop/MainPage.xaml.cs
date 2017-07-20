6d5929c351f8e58e48b7842962971b0b2750f072
Andrew Wang <w.andrew195@windowslive.com>
2017-07-20 15:35:00 +1200

    Camera Working

--- SnapShop.v2/SnapShop/SnapShop/SnapShop/MainPage.xaml.cs
+++ SnapShop.v2/SnapShop/SnapShop/SnapShop/MainPage.xaml.cs
@@ -18,9 +18,33 @@ namespace SnapShop
             image.Source = ImageSource.FromFile("logo.png");
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            MediaFile file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                PhotoSize = PhotoSize.Medium,
+                Directory = "Sample",
+                Name = $"{DateTime.UtcNow}.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                return file.GetStream();
+            });
+
+            file.Dispose();
+
         }
     }
 }
